9b7f3e8e724fd5c588eb8450b546c3d7d09f03db
EHOR-FT <ovallee040@gmail.com>
2024-08-08 14:56:20 -0500

    informacion.php

--- informacion.php
+++ informacion.php
@@ -1,19 +1,10 @@
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-<body>
 <?php
 $Identificacion= $_POST ['Identificacion'];
 $NomApellidos=$_POST ['NomApellidos'];
 $Cargo=$_POST ['Cargo'];
 $Salario=$_POST ['Salario'];
-echo "La identificacion del Empleado es: ".Identificacion;
-echo "Los nombres y apellidos del empleado son: ".NomApellidos;
-echo "El cargo del empleado es: ".Cargo;
-echo "El salarios del empleado es: ".Salario;
+echo "La identificacion del Empleado es: ".$Identificacion;
+echo "Los nombres y apellidos del empleado son: ".$NomApellidos;
+echo "El cargo del empleado es: ".$Cargo;
+echo "El salario del empleado es: ".$Salario;
 ?>
-</body>
-</html>
-
